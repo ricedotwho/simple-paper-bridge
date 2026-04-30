a819aa126a4217bb878dbac809d63b44015f04f8
rice.who <100787688+ricedotwho@users.noreply.github.com>
2026-05-01 00:21:51 +1000

    fix prio and ignore cancelled

--- src/main/java/com/ricedotwho/paperbridge/ChatListener.java
+++ src/main/java/com/ricedotwho/paperbridge/ChatListener.java
@@ -3,13 +3,14 @@ package com.ricedotwho.paperbridge;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class ChatListener implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void bruh(AsyncChatEvent event) {
         String content = PlainTextComponentSerializer.plainText().serialize(event.message());
         BridgePlugin.onMinecraftMessage(event.getPlayer().getName(), content);
